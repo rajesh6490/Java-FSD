10e4fabc0b424bfc2f5846d86b620c38c2eba573
Rajesh I <rajesh.in1209@gmail.com>
2026-06-20 22:50:44 +0530

    Newlyy added that Java-Rv Files of that

---
+++ Java-Rv/Var.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Var{
+    public static void main(String args[]){
+
+        // Day 01
+        // Variables
+        // 1. Camel case
+
+        String camelCase;
+        camelCase = """
+        Rajesh!!
+        This is the Camel case!
+        """;
+        System.out.println(camelCase);
+
+
+        // 2. Pascal case
+        String PascalCase;
+        PascalCase = """
+        This is The Pascal case of Thhat Text """;
+        System.out.println(PascalCase);
+      System.out.println("\nthis is The Pascal Case Text"+PascalCase);
+
+    // 3. UPPER_SNAKED CASE
+
+    String UPPER_SNAKED;
+    UPPER_SNAKED = """
+    This is The Uppper Snaked Case""";
+
+    System.out.println(UPPER_SNAKED);
+        
+
+        //4. lwr case and Upr 
+
+        String firstname;
+        String LASTNAME;
+
+        firstname = """
+        This is The First Name""";
+
+        LASTNAME = """
+        THIS IS THE LAST NAME!""";
+
+        System.out.println((firstname)+LASTNAME);
+
+
+        // 5. Snaked_Case
+
+        String Snaked_Case;
+
+        Snaked_Case = """
+        This is an Snaked case 
+        """;
+
+        System.out.println(Snaked_Case);
+    }
+}
---
+++ Java-Rv/dat.java
@@ -0,0 +1,100 @@
+import java.util.*;
+public class Var{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // int a;
+        // Float b;
+        // String c;
+        // System.out.println("Names Student! ");
+        // System.out.println("Enter id no ");
+        // a = sc.nextInt();
+        // System.out.println("Enter Marks: ");
+        // b = sc.nextFloat();
+        // sc.nextLine();
+        // System.out.println("Enter Text: ");
+        // c = sc.nextLine();
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+
+
+
+        // float c,x,y,z,sv;
+        // System.out.println("Enter No X value : ");
+        // x = sc.nextFloat();
+        // sc.nextLine();
+        // System.out.println("Enter No y value : ");
+        // y = sc.nextFloat();
+        // sc.nextLine();
+        // System.out.println("Enter No Z value : ");
+        // z = sc.nextFloat();
+        // sc.nextLine();
+
+
+        // c = ((x*x)+(y*y)+(z*z));
+
+        //  sv = (float) Math.sqrt(c);
+
+        // System.out.println(sv);
+        
+        
+
+        // float d1,d2;
+        // System.out.println("Enter D1: ");
+        // d1 = sc.nextFloat();
+        // System.out.println("Enter D2: ");
+        // d2 = sc.nextFloat();
+
+        // float a = (float) (0.5)*(d1*d2);
+
+        // System.out.println(a);
+        
+        // float a,b,h;
+        // System.out.println("Enter no b,h");
+        // b=sc.nextFloat();
+        // h=sc.nextFloat();
+
+        // a = (float) 0.5 * (b*h);
+
+        // System.out.println(a);
+
+        // float h,a,b,z;
+        // System.out.println("Enter no h,a,b");
+        // h=sc.nextFloat();
+        // a=sc.nextFloat();
+        // b=sc.nextFloat();
+
+        // z =  0.5f * h * (a+b);
+
+        // System.out.println(z);
+
+
+
+        // all data using bankk Detgails 
+    
+        int a;
+        char[] c = new char[20];
+        
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
