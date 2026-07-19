bc2398507bb08c457b3d4ea85626cdafdfac7e53
Ansh_A <rajesh.in1209@gmail.com>
2026-07-19 23:26:26 +0530

    Update server.js

--- Postgres/server.js
+++ Postgres/server.js
@@ -6,26 +6,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.post('/employees', async (req, res) => {
-    const { name, email, salary } = req.body;
-    await pool.query(
-        'INSERT INTO employee(name,email,salary) VALUES($1,$2,$3)',
-        [name, email, salary]
-    );
-    res.send("Employee Added");
+const { name, email, salary } = req.body;
+await pool.query(
+INSERT INTO employee(name,email,salary) VALUES($1,$2,$3)',
+    [name, email, salary]
+);
+res.send("Employee Added");
 });
 app.get('/employees', async (req, res) => {
-    const result = await pool.query(
-        'SELECT * FROM employee ORDER BY id'
-    );
-    res.json(result.rows);
+const result = await pool.query(
+'SELECT * FROM employee ORDER BY id'
+ );
+res.json(result.rows);
 });
 app.put('/employees/:id', async (req, res) => {
-    const { name, email, salary } = req.body;
-    await pool.query(
-        'UPDATE employee SET name=$1,email=$2,salary=$3 WHERE id=$4',
-        [name, email, salary, req.params.id]
-    );
-    res.send("Employee Updated");
+const { name, email, salary } = req.body;
+await pool.query(
+'UPDATE employee SET name=$1,email=$2,salary=$3 WHERE id=$4',
+[name, email, salary, req.params.id]
+);
+res.send("Employee Updated");
 });
 app.delete('/employees/:id', async (req, res) => {
     await pool.query(
